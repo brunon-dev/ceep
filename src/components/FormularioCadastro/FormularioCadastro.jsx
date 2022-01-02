403a5d9d2987c19564731efe0b5a60c305b512d1
Bruno Figueiredo Nogueira <bruno@brunon.com.br>
2022-01-01 23:21:41 -0300

    Cria função manipuladora de evento para mudança de título

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 import "./estilo.css";
 
 export default class FormularioCadastro extends Component {
+
+  constructor(){
+    super();
+    this.titulo = "";
+  }
+
+  handleMudancaTitulo(evento){
+    this.titulo = evento.target.value;
+    console.log(this.titulo);
+  }
+  
   render() {
     return (
       <form className="form-cadastro">
@@ -9,6 +20,7 @@ export default class FormularioCadastro extends Component {
           type="text"
           placeholder="Título"
           className="form-cadastro_input"
+          onChange={this.handleMudancaTitulo.bind(this)}
         />
         <textarea
           rows={15}
